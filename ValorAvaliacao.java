bcee2789dbd9f6a53164befac64ab05a480fb29d
PauloMelgarejo <50964371+PauloMelgarejo@users.noreply.github.com>
2019-05-23 14:37:30 -0400

    Update ValorAvaliacao.java

--- ValorAvaliacao.java
+++ ValorAvaliacao.java
@@ -1,14 +1,20 @@
 public class ValorAvaliacao {
 	private float valor;
-	private MedidaAvaliacao medida;
+	private MedidaAvaliacao medida = new MedidaAvaliacao();
 
 	public ValorAvaliacao(MedidaAvaliacao medida) {
 		valor = 0.0f;
 		this.medida = medida;
 	}
 
-	public void setValor(float valor) {
+	public boolean setValor(float valor) {
 		this.valor = valor;
+		if (getMenorValor < valor < getMaiorValor){
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	public float getValor() {
