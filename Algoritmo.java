1312f7bf78e6d9fa3bb028cfa11b97e9b8824a6a
strawkeiji <strawkeiji@gmail.com>
2019-05-26 19:25:21 -0400

    'teste'

--- Algoritmo.java
+++ Algoritmo.java
@@ -3,18 +3,19 @@ public class Algoritmo {
 	private String nome;
 	private float [] parametros = new float[3];
 	
-	public Algoritmo(String nome,float parametros[]) { //Fazer validação desse vetor
+	public Algoritmo(String nome,float parametros[]) { 
 		this.nome = nome;
-		for (int i =0;i<3;i++ ) {
-			if (parametros[i]==0.0f) {
+		for (int i = 0; i < 3; i++ ) {
+			if (parametros[i] == 0.0f) {
 				this.parametros[i] =  Float.MAX_VALUE;			
 			}			
 		}		
-
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public float [] getParametros() {
 		return parametros;
 	}
